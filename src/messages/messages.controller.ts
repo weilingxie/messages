799c18bdcef5cd1299e5943b8070a0c28991f708
weilingxie1 <sqlnever@gmail.com>
2022-05-23 18:11:21 +1200

    Added controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { Message } from './type';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return [
+      { content: 'message1' },
+      { content: 'message2' },
+      { content: 'message3' },
+    ];
+  }
+
+  @Post()
+  createMessage(@Body() body: Message) {
+    console.log(body.content + ' created');
+  }
+
+  @Get('/:id')
+  getMessageById(@Param() id: number) {
+    const message: Message = { content: `message${id}` };
+    console.log(id);
+    return message;
+  }
+}
